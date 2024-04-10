39641d21760a59900fa988d14e7bda64a9e5bcc3
Daniel <danielmathers97@gmail.com>
2024-04-10 13:56:34 +0200

    installled tailwind intellisense for className autocompletes

--- src/tailWindSandbox/styleComponents.js
+++ src/tailWindSandbox/styleComponents.js
@@ -1,9 +1,9 @@
 export default function MyCustomButton() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline bg-blue-500">
-        tHIS IS A TEST
-      </h1>
+      <button className="bg-sky-500 px-5 py-2 border rounded-full max-w-fit text-white hover:bg-sky-600 outline-none hover:ring-2">
+        Save
+      </button>
     </>
   );
 }
