969c361f5a6965d7f47da119809da6c96e8f68a0
Daniel <danielmathers97@gmail.com>
2024-04-10 12:29:34 +0200

    tailwind has been successfully setup

--- src/tailWindSandbox/styleComponents.js
+++ src/tailWindSandbox/styleComponents.js
@@ -1,7 +1,9 @@
 export default function MyCustomButton() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">tHIS IS A TEST</h1>
+      <h1 className="text-3xl font-bold underline bg-blue-500">
+        tHIS IS A TEST
+      </h1>
     </>
   );
 }
