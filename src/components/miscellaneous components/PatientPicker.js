252eaa332617f6ca6118bfff3d1383c0dab7adf2
Daniel <danielmathers97@gmail.com>
2024-04-16 17:17:33 +0200

    added notifications compoenet to encapuslate email logic. imporved appoitnmenttype picker ui.  added to call backs in apptype and patint picker so that on selection the compoennts dismount. added global profile data to avoid reptitve api calls. check todos for email notficiation compoenent

--- src/components/miscellaneous components/PatientPicker.js
+++ src/components/miscellaneous components/PatientPicker.js
@@ -57,11 +57,11 @@ export default function PatientPicker({ profileId, hideComponent, onclick }) {
                 .map((patient) => (
                   <div
                     key={patient.id}
-                    className="border-b border-slate-300 flex peer/patientDiv font-semibold px-3 py-2 hover:bg-slate-100 ease-in-out duration-[20ms]"
+                    className="border-b group border-slate-300 flex  items-center font-semibold px-3 py-2 hover:bg-slate-100 ease-in-out duration-[20ms]"
                     onClick={() => onclick(patient)}
                   >
-                    {patient?.first_name || ""} {patient?.last_name || ""}/
-                    <p className="text-xs peer-hover/patientDiv:inline text-slate-200 hidden  ">{`${
+                    {patient?.first_name || ""} {patient?.last_name || ""}
+                    <p className="text-xs ml-1 hidden  text-slate-500 group-hover:block ">{`${
                       patient?.contact_number || ""
                     }/${patient?.email}`}</p>
                   </div>
