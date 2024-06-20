31637fcbcf3da5c3495524de563db90c31007afb
Daniel <danielmathers97@gmail.com>
2024-06-20 16:57:44 +0200

    added settings dropdown with animation. animation needs work. removed create appointment and create patient from sidebar. added user greeting header

--- src/components/mainMenuSideBar/MainMenuSideBar.js
+++ src/components/mainMenuSideBar/MainMenuSideBar.js
@@ -1,47 +1,76 @@
-import { Link } from "react-router-dom";
 import SideBarItemsWithIconDiv from "../miscellaneous components/SideBarItemsWithIcon";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
+import SettingsIcon from "@mui/icons-material/Settings";
+import { motion, AnimatePresence } from "framer-motion";
+import { useGlobalStore } from "../../zustandStore/store";
+import { duration } from "@mui/material";
 
-export default function MainMenuSideBar({ itemsArray }) {
+export default function MainMenuSideBar() {
+  const [showDropDownSettings, setShowDropDownSettings] = useState(false);
+  const { globalProfileData } = useGlobalStore();
   return (
     <>
-      <div className="min-w-full bg-slate-500 h-fit   sticky left-0 top-0 bottom-0 flex flex-col flex-wrap justify-center   ">
-        <div className=" pl-4 flex items-center space-x-1 text-nowrap  ">
-          <p>
-            <FontAwesomeIcon
-              icon="fa-solid fa-bars"
-              size="2xl"
-              style={{ color: "whitesmoke" }}
-            />
-          </p>
-          <h1 className="text-2xl  text-white p-3 ">Schedule Sensai</h1>
+      <div className="min-w-full bg-slate-500 h-full  sticky left-0 top-0 bottom-0 flex flex-col text-nowrap  ">
+        <div className=" flex items-center pl-4  mb-5  py-2">
+          <h2 className="text-2xl text-white">
+            Welcome{" "}
+            {(globalProfileData?.first_name || "") +
+              " " +
+              (globalProfileData?.last_name || "")}
+          </h2>
         </div>
-        <SideBarItemsWithIconDiv
-          icon={<FontAwesomeIcon icon="fa-solid fa-user" size="lg" />}
-          linkText="Profile"
-          linkTo="profile"
-        />
-
-        <SideBarItemsWithIconDiv
-          icon={<FontAwesomeIcon icon="fa-solid fa-house-medical" size="lg" />}
-          linkText="Practice"
-          linkTo={"practice"}
-        />
         <SideBarItemsWithIconDiv
           linkText={"Patient List"}
           linkTo={"patient/search"}
           icon={<FontAwesomeIcon icon="fa-solid fa-rectangle-list" size="lg" />}
         />
-        <SideBarItemsWithIconDiv
-          linkText={"Create Patient"}
-          linkTo={"patientCreate"}
-          icon={<FontAwesomeIcon icon="fa-solid fa-square-plus" size="lg" />}
-        />
-        <SideBarItemsWithIconDiv
-          linkText={"Create appointment"}
-          linkTo={"createAppointment"}
-          icon={<i class="fa-solid fa-circle-plus fa-lg"></i>}
-        />
+        <div
+          className="relative"
+          onClick={() => setShowDropDownSettings(!showDropDownSettings)}
+        >
+          <SideBarItemsWithIconDiv
+            icon={<SettingsIcon />}
+            linkText="Profile settings"
+          />
+          <div className="absolute right-3  top-1/2 transform -translate-y-1/2">
+            <FontAwesomeIcon
+              icon={`fa-solid ${
+                showDropDownSettings ? "fa-chevron-up" : "fa-chevron-down"
+              } `}
+              style={{ color: "white" }}
+              size="lg"
+            />
+          </div>
+        </div>
+        <AnimatePresence>
+          {showDropDownSettings && (
+            <motion.div
+              initial={{ height: 0, zIndex: -1 }}
+              animate={{
+                height: "auto",
+                zIndex: 1,
+              }}
+              transition={{ duration: 0.1 }}
+              className="flex flex-col justify-center border-b border-white pb-2 "
+            >
+              <SideBarItemsWithIconDiv
+                icon={<FontAwesomeIcon icon="fa-solid fa-user" size="lg" />}
+                linkText="Profile"
+                linkTo="profile"
+              />
+
+              <SideBarItemsWithIconDiv
+                icon={
+                  <FontAwesomeIcon icon="fa-solid fa-house-medical" size="lg" />
+                }
+                linkText="Practice"
+                linkTo={"practice"}
+              />
+            </motion.div>
+          )}
+        </AnimatePresence>
+
         <SideBarItemsWithIconDiv
           linkText={"Appointment Type Portal"}
           linkTo={"appointmentTypePortal"}
